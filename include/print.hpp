547d1ff98fa339a413fce1b08cef6b58bb399ac1
vaulex <krasauchek.s.iy@ya.ru>
2018-02-05 11:52:58 +0300

    added documentation

--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout);	//конструктор1
-void print(const std::string& text, std::ofstream& out);	//конструктор2
